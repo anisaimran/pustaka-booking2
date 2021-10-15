9968de886a0cc8a24a99aad092c27ca77446977d
anisaimran_github <anisaaimran77@gmail.com>
2021-10-15 11:07:26 +0700

    menambahkan validasi data dengan form validasi pada controller matakuliah

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -56,9 +56,9 @@ $autoload['packages'] = array();
 | You can also supply an alternative library name to be assigned
 | in the controller:
 |
-|	$autoload['libraries'] = array();
+|	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation');
+$autoload['libraries'] = array(); $autoload['libraries'] = array('form_validation');
 
 /*
 | -------------------------------------------------------------------
