dad4044d087015cb5c081bdec3cfd0a5a7a8aff0
anisaimran_github <anisaaimran77@gmail.com>
2021-12-08 18:04:28 +0700

    pertemuan 11

--- application/controllers/buku.php
+++ application/controllers/buku.php
@@ -1,11 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-
-
-
-
-
-
-
+class Buku extends CI_Controller
+{
+ public function __construct()
+ {
+ parent::__construct();
         cek_login();
     }
 
